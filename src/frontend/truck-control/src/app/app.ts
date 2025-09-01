ee2f9c19b75c30891bbbbaf79d904454fc905268
Jessica Meira <jessica.meira1@gmail.com>
2025-09-01 14:21:15 -0300

    remove unecessary reference

--- src/frontend/truck-control/src/app/app.ts
+++ src/frontend/truck-control/src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Footer } from './shared/components/footer/footer'; 
-import { ListTrucks } from './modules/trucks/views/pages/list-trucks/list-trucks';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, ListTrucks],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
